55945ccf74053edccb73850422c29cfc15f55919
mccoylin <mccoy.lin@gmail.com>
2024-04-19 14:29:43 +0800

    support STM32CubeIDE
    1. __nop() change to __NOP()

--- tm1637.c
+++ tm1637.c
@@ -28,7 +28,10 @@ void tm1637_delay_us(uint8_t delay)
   while (delay > 0)
   {
     delay--;
-    __nop();__nop();__nop();__nop();
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
   }
 }
 //#######################################################################################################################
@@ -56,9 +59,13 @@ uint8_t tm1637_write_byte(tm1637_t *tm1637, uint8_t data)
     HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_RESET);
     tm1637_delay_us(_TM1637_BIT_DELAY);
     if (data & 0x01)
+    {
       HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_SET);
+    }
     else
+    {
       HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+    }
     tm1637_delay_us(_TM1637_BIT_DELAY);
     HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_SET);
     tm1637_delay_us(_TM1637_BIT_DELAY);
@@ -72,7 +79,9 @@ uint8_t tm1637_write_byte(tm1637_t *tm1637, uint8_t data)
   tm1637_delay_us(_TM1637_BIT_DELAY);
   uint8_t ack = HAL_GPIO_ReadPin(tm1637->gpio_dat, tm1637->pin_dat);
   if (ack == 0)
+  {
     HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+  }
   tm1637_delay_us(_TM1637_BIT_DELAY);
   HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_RESET);
   tm1637_delay_us(_TM1637_BIT_DELAY);
@@ -82,7 +91,9 @@ uint8_t tm1637_write_byte(tm1637_t *tm1637, uint8_t data)
 void tm1637_lock(tm1637_t *tm1637)
 {
   while (tm1637->lock == 1)
+  {
     tm1637_delay_ms(1);
+  }
   tm1637->lock = 1;  
 }
 //#######################################################################################################################
@@ -123,9 +134,13 @@ void tm1637_brightness(tm1637_t *tm1637, uint8_t brightness_0_to_7)
 void tm1637_write_raw(tm1637_t *tm1637, const uint8_t *raw, uint8_t length, uint8_t pos)
 {
   if (pos > 5)
+  {
     return;
+  }
   if (length > 6)
+  {
     length = 6;
+  }
   // write COMM1
   tm1637_start(tm1637);
   tm1637_write_byte(tm1637, TM1637_COMM1);
@@ -135,7 +150,9 @@ void tm1637_write_raw(tm1637_t *tm1637, const uint8_t *raw, uint8_t length, uint
   tm1637_write_byte(tm1637, TM1637_COMM2 + (pos & 0x03));
   // write the data bytes
   for (uint8_t k=0; k < length; k++)
+  {
     tm1637_write_byte(tm1637, raw[k]);
+  }
   tm1637_stop(tm1637);
   // write COMM3 + brightness
   tm1637_start(tm1637);
@@ -159,9 +176,13 @@ void tm1637_write_int(tm1637_t *tm1637, int32_t digit, uint8_t pos)
   for (uint8_t i=0; i < 6; i++)
   {
     if (str[i] == '-')
+    {
       buffer[i] = _tm1637_minus;
+    }
     else if((str[i] >= '0') && (str[i] <= '9'))
+    {
       buffer[i] = _tm1637_digit[str[i] - 48];
+    }
     else
     {
       buffer[i] = 0;
@@ -178,39 +199,51 @@ void tm1637_write_float(tm1637_t *tm1637, float digit, uint8_t floating_digit, u
   char str[8];
   uint8_t buffer[6] = {0};
   if (floating_digit >6)
+  {
     floating_digit = 6;
+  }
   switch (floating_digit)
   {
     case 0:
       snprintf(str, sizeof(str) , "%.0f", digit);
-    break;
+      break;
+
     case 1:
       snprintf(str, sizeof(str) , "%.1f", digit);
-    break;
+      break;
+
     case 2:
       snprintf(str, sizeof(str) , "%.2f", digit);
-    break;
+      break;
+
     case 3:
       snprintf(str, sizeof(str) , "%.3f", digit);
-    break;
+      break;
+
     case 4:
       snprintf(str, sizeof(str) , "%.4f", digit);
-    break;
+      break;
+
     case 5:
       snprintf(str, sizeof(str) , "%.5f", digit);
-    break;
+      break;
+
     case 6:
       snprintf(str, sizeof(str) , "%.6f", digit);
-    break;
+      break;
   } 
   if (tm1637->show_zero == false)
   {
     for (int8_t i = strlen(str) - 1; i > 0; i--)
     {
       if (str[i] == '0')
+      {
         str[i] = 0;
+      }
       else
+      {
         break;            
+      }
     }
   }
   uint8_t index = 0;  
@@ -229,7 +262,9 @@ void tm1637_write_float(tm1637_t *tm1637, float digit, uint8_t floating_digit, u
     else if (str[i] == '.')
     {
       if (index > 0)
+      {
         buffer[index - 1] |= _tm1637_dot;      
+      }
     }
     else
     {
@@ -249,18 +284,14 @@ void tm1637_show_zero(tm1637_t *tm1637, bool enable)
 void tm1637_fill(tm1637_t *tm1637, bool enable)
 {
 	if (enable)
+  {
 		tm1637_write_segment(tm1637, _tm1637_on, 6, 0);
+  }
 	else
+  {
 		tm1637_write_segment(tm1637, _tm1637_off, 6, 0);		
+  }
 }
 //#######################################################################################################################
 
 //#######################################################################################################################
-
-
-
-
-
-
-
-
